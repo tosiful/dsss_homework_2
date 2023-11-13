1904ccfc9b6b80b70225dac7e573affd59a6c404
Tosiful Islam <tosifulislam101@gmail.com>
2023-11-13 19:20:33 +0100

    a new brach opend

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -12,31 +12,34 @@ def function_B():
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
+def function_C(x1, x2, o):#changed  the varibale name 
+    p = f"{x1} {o} {x2}"
+    if o == '+': a = x1 + x2
+    elif o == '-': a = x1 - x2
+    else: a = x1 * x2
     return p, a
 
 def math_quiz():
     s = 0
-    t_q = 3.14159265359
+    t_q = int (3.1416) # changed float to int
+
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        x1 = function_A(1, 10);
+        x2 = function_A(1, 5); #changed the value(5.5)float to int
+        o = function_B()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = function_C(x1, x2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            s += 1 #modified this variable
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
